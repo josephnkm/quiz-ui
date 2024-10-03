12e1c2d1f4f8170cfff488c9a6da9d15fde98f81
JOSEPH NAKARMI <josephnkm@gmail.com>
2024-10-04 01:37:02 +0545

    updated backend api endpoint

--- src/Utils/endpoints.js
+++ src/Utils/endpoints.js
@@ -1,4 +1,4 @@
-export const baseURL = "http://localhost:8080/api";
+export const baseURL = "https://quiz-5v4p.onrender.com/api";
 
 export const GET_ALL_QUIZ_URL = `${baseURL}/quizzes`;
 export const UPDATE_QUIZ_URL = `${baseURL}/update-mcq/{questionId}`;
@@ -6,5 +6,5 @@ export const SAVE_ALL_QUIZ_URL = `${baseURL}/save-mcqs`;
 export const PARSE_TEXT_TO_QUIZ_MCQ_URL = `${baseURL}/parse-text`;
 
 // 7HphnIpuOYowX6sr
-// joseph00
+// joseph00 > sAYdZCQkUqnHHhmN
 // mongodb+srv://joseph:<db_password>@quiz-cluster.omwii.mongodb.net/?retryWrites=true&w=majority&appName=quiz-cluster
